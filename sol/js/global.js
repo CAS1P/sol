010e83f8fdfe7e509ec709543e29a68f0185f0a9
CAS1P <zacharias.peterssonivarsson07@skola.stenungsund.se>
2026-02-16 15:33:00 +0100

    sol

---
+++ sol/js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.sol = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(Drupal);
